c50c32c5fb4bd57699e809af7c2db13a5ce78902
BalazsBarna <balazsbarni@gmail.com>
2017-09-11 23:37:37 +0200

    EndedEx17

---
+++ week02/day01/17-average-of-input.py
@@ -0,0 +1,14 @@
+# Write a program that asks for 5 integers in a row,
+# then it should print the sum and the average of these numbers like:
+#
+# Sum: 22, Average: 4.4
+
+input_1 = int(input("Nr.1= "))
+input_2 = int(input("Nr.2= "))
+input_3 = int(input("Nr.3= "))
+input_4 = int(input("Nr.4= "))
+input_5 = int(input("Nr.5= "))
+
+output_sum = input_1 + input_2 + input_3 + input_4 + input_5
+output_avg = input_1 * input_2 * input_3 * input_4 * input_5
+print("Sum: " , output_sum , "Average: " , output_avg )
