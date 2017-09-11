9e34d5e0ca9c92e12d67ea8461648446ccffcdc2
BalazsBarna <balazsbarni@gmail.com>
2017-09-11 23:41:35 +0200

    EndedEx20

---
+++ week02/day01/20-print-bigger.py
@@ -0,0 +1,9 @@
+# Write a program that asks for two numbers and prints the bigger one
+
+input_1 = int(input("Nr.1= "))
+input_2 = int(input("Nr.2= "))
+
+if input_1 < input_2:
+    print(input_2)
+else:
+    print(input_1)    
