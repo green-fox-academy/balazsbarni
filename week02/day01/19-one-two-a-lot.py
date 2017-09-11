7cecf6617b86d992d521bb17fa9afb8fb98dd24f
BalazsBarna <balazsbarni@gmail.com>
2017-09-11 16:11:28 +0200

    EndedEx19

---
+++ week02/day01/19-one-two-a-lot.py
@@ -0,0 +1,15 @@
+# Write a program that reads a number form the standard input,
+# If the number is zero or smaller it should print: Not enough
+# If the number is one it should print: One
+# If the number is two it should print: Two
+# If the number is more than two it should print: A lot
+
+x = float(input("Number: "))
+if x <= 0:
+    print("Not enough!")
+elif x == 1:
+    print("One")
+elif x == 2:
+    print("Two")
+elif x > 2:
+    print("A lot") 
