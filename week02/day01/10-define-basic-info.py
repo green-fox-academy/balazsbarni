4d43b5803be13ae31fbd0bcd7dbedeb084c2d584
BalazsBarna <balazsbarni@gmail.com>
2017-09-11 14:08:40 +0200

    EndedEx10

---
+++ week02/day01/10-define-basic-info.py
@@ -0,0 +1,17 @@
+# Define several things as a variable than print their values
+# Your name as a string
+# Your age as an integer
+# Your height in meters as a double
+# Wether you are married or not as a boolean
+
+name = "Barni"
+print(name)
+
+age = 26
+print(age)
+
+height = 1.78
+print(height)
+
+married = False
+print(married)
