5ad3f9d08741911f7a6013409a9d03aea540c15a
BalazsBarna <balazsbarni@gmail.com>
2017-09-12 17:21:08 +0200

    EndedEx16

---
+++ week02/day02/16-append-a.py
@@ -0,0 +1,10 @@
+# - Create a variable named `nimals`
+#   with the following content: `["kuty", "macsk", "cic"]`
+# - Add all elements an `"a"` at the end
+
+nimals = ["kuty", "macsk", "cic"]
+
+for i in nimals:
+    print(i + "a")
+
+
