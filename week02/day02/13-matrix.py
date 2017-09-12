88658d3f7467e275dd1f0b98c5cbcb45041779cb
BalazsBarna <balazsbarni@gmail.com>
2017-09-12 16:11:20 +0200

    Part1Ex13Ended

---
+++ week02/day02/13-matrix.py
@@ -0,0 +1,25 @@
+# - Create (dynamically) a two dimensional list
+#   with the following matrix. Use a loop!
+#
+#   1 0 0 0
+#   0 1 0 0
+#   0 0 1 0
+#   0 0 0 1
+#
+# - Print this two dimensional list to the output
+
+def list_matrix (widht, height):
+    height_list = []
+    for i in range(widht):
+        widht_list = []
+        for j in range(height):
+            
+                widht_list.append ("0")
+        height_list.append (widht_list)
+    return height_list
+
+def row_matrix (list_matrix):
+    for row in list_matrix:
+        print (row)
+
+row_matrix(list_matrix(4,5))
