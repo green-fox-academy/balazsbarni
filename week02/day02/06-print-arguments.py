3eb57a08ad162c3fe331b3d051f4fa551d1c6207
BalazsBarna <balazsbarni@gmail.com>
2017-09-12 12:04:11 +0200

    EndedEx6

---
+++ week02/day02/06-print-arguments.py
@@ -0,0 +1,8 @@
+# - Create a function called `printer`
+#   which prints the input parameters
+#   (can have multiple number of arguments)
+
+def printer (*printable):
+    print (printable)
+
+printer("sad", "asd")
