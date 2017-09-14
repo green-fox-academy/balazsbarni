342a9af41f6e17231af7c8f6ec8f9cc81f0f4c18
BalazsBarna <balazsbarni@gmail.com>
2017-09-14 16:12:42 +0200

    EndedExDictionaries3

--- week02/day03/03-dictionaries.py
+++ week02/day03/03-dictionaries.py
@@ -1,7 +1,7 @@
 
 accounts = [
-	{ 'client_name': 'Igor', 'account_number': 11234543, 'balance': 203004099.2 },
-	{ 'client_name': 'Vladimir', 'account_number': 43546731, 'balance': 5204100071.23 },
+	{ 'client_name': 'Igor', 'account_number': 1, 'balance': 203004099.2 },
+	{ 'client_name': 'Vladimir', 'account_number': 2, 'balance': 5204100071.23 },
 	{ 'client_name': 'Sergei', 'account_number': 23456311, 'balance': 1353600.0 }
 ]
 
@@ -14,13 +14,39 @@ accounts = [
 #  - amount to transfer
 #
 # Print "404 - account not found" if any of the account numbers don't exist
-acc_num = int(input("Szamlaszam: "))
+#acc_num = int(input("Szamlaszam: "))
 
-def balance(items):
-    for i in items:
+#def balance(items):
+ #   for i in items:
+  #      if i['account_number'] == acc_num:
+   #         print(i['client_name'], i['balance'])
+
+
+
+acc_num = int(input("Szamlaszamod: "))
+to_acc_num = int(input("Ahova utalni akarsz: "))
+amm_num = int(input("Amennyit utalsz: "))
+
+def transfer_money(accounts_list, acc_number):
+    for i in accounts_list:
         if i['account_number'] == acc_num:
-            print(i['client_name'], i['balance'])
+            take_money(acc_num, amm_num, accounts)
+            add_money(to_acc_num, amm_num, accounts)
+    print(accounts)
+        
+def take_money(from_id, by_ammount, accounts_list):
+    for i in accounts_list:
+        if i["account_number"] == from_id:
+            i["balance"] -= by_ammount
+
+
+def add_money(to_id, by_ammount, accounts_list):
+    for i in accounts_list:
+        if i["account_number"] == to_id:
+            i["balance"] += by_ammount
 
 
 
-balance(accounts)
+#balance(accounts)
+
+transfer_money(accounts, acc_num)
