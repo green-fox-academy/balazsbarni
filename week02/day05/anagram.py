3eb53b63fe53917075ed109ac208d5522e4cc193
BalazsBarna <balazsbarni@gmail.com>
2017-09-15 10:55:15 +0200

    Fix(Anagram)nested in function

--- week02/day05/anagram.py
+++ week02/day05/anagram.py
@@ -1,13 +1,14 @@
 first_word = input("Enter the first word: ")
 second_word = input("Enter the second word: ")
 
-word_list_first = list(first_word)
-word_list_second = list(second_word)
 
-if word_list_first == word_list_second:
-    print("True")
-else:
-    print("False")
- 
+def check_if_anagram(first_input, second_input):
+    word_list_first = sorted(list(first_input))
+    word_list_second = sorted(list(second_input))
+    if word_list_first == word_list_second:
+        return True
+    else:
+        return False
+    
 
-        
+print(check_if_anagram(first_word, second_word))
