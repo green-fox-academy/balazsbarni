b026f2b4584052d5df1d47375bdcf96f449bba2e
BalazsBarna <balazsbarni@gmail.com>
2017-09-15 10:57:27 +0200

    Fixed(PalindtomeBuilder)nested in function

--- week02/day05/palindrome-builder.py
+++ week02/day05/palindrome-builder.py
@@ -1,3 +1,7 @@
 to_palindrome_word = input("Enter the word: ")
 
-print(to_palindrome_word, to_palindrome_word[::-1] , sep='')
+
+def build_palindrome(entered_word):
+    print(entered_word, entered_word[::-1] , sep='')
+
+build_palindrome(to_palindrome_word)
