6007ce670d5a6114849455b6c8696504ef61e436
BalazsBarna <balazsbarni@gmail.com>
2017-09-15 10:26:46 +0200

    EndedAnagramExample

---
+++ week02/day05/anagram.py
@@ -0,0 +1,13 @@
+first_word = input("Enter the first word: ")
+second_word = input("Enter the second word: ")
+
+word_list_first = list(first_word)
+word_list_second = list(second_word)
+
+if word_list_first == word_list_second:
+    print("True")
+else:
+    print("False")
+ 
+
+        
