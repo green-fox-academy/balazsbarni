16056db38d9c4d1674f4c7a452f773981ceb609d
BalazsBarna <balazsbarni@gmail.com>
2017-09-15 12:07:25 +0200

    EndedExArmstrongNumber

--- week02/day05/armstrongs-number.py
+++ week02/day05/armstrongs-number.py
@@ -1,6 +1,10 @@
-given_number = input("Give the number, you want to check: ")
+given_number = int(input("Give the number, you want to check: "))
 
-
+def is_armstrong(number_to_check):
+    if number_to_check == armstrong_sum(number_to_check):
+        print("It is an Armstrong number!")
+    else:
+        print("It is not an Armstrong number!")
 
 def length_of(lenght_of_a_number):
     return len(str(lenght_of_a_number))
@@ -8,12 +12,12 @@ def length_of(lenght_of_a_number):
 #print(length_of(given_number))
     
 
-def is_armstrong(number_to_check):
+def armstrong_sum(number_to_check):
     armstrong_sum = 0
     for digit in map(int, str(number_to_check)):
         armstrong_sum += digit ** length_of(number_to_check)
-    print(armstrong_sum)
-
+    return armstrong_sum
+    
 
 is_armstrong(given_number) 
 
