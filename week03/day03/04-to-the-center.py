c0cf79633ff65510cd1a7ee05b1214a974eae49b
BalazsBarna <balazsbarni@gmail.com>
2017-09-20 12:10:50 +0200

    AddedFunction

--- week03/day03/04-to-the-center.py
+++ week03/day03/04-to-the-center.py
@@ -15,9 +15,14 @@ x_one, x_two = 280, 10
 y_one, y_two = 120, 50
 z_one, z_two = 200, 250
 
-line_first = canvas.create_line(x_one, x_two, (width / 2), (height / 2))
-line_second = canvas.create_line(y_one, y_two, 150, 150)
-line_third = canvas.create_line(z_one, z_two, 150, 150)
+def line_draw_to_centre(par_one, par_two):
+    line_to_center = canvas.create_line(par_one, par_two, width/2, height/2)
 
+#line_first = canvas.create_line(x_one, x_two, (width / 2), (height / 2))
+#line_second = canvas.create_line(y_one, y_two, 150, 150)
+#line_third = canvas.create_line(z_one, z_two, 150, 150)
 
+line_draw_to_centre(x_one, x_two)
+line_draw_to_centre(y_one,y_two)
+line_draw_to_centre(z_one, z_two)
 root.mainloop()
