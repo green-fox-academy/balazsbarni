9048010f1ffeb617d91ca8f6b20ad2c427cd9f79
BalazsBarna <balazsbarni@gmail.com>
2017-09-25 11:58:58 +0200

    feat(PostIt):ExampleEnded

---
+++ week04/01-post-it.py
@@ -0,0 +1,25 @@
+class PostIt(object):
+   background_color = ""
+   text = ""
+   text_color = ""
+
+
+ex_one = PostIt()
+ex_two = PostIt()
+ex_three = PostIt()
+
+ex_one.background_color = "orange"
+ex_one.text = "Idea 1"
+ex_one.text_color = "blue"
+ex_two.background_color = "pink"
+ex_two.text = "Awsome"
+ex_two.text_color = "black"
+ex_three.background_color = "yellow"
+ex_three.text = "Superb!"
+ex_three.text_color = "green"
+
+
+print(ex_three.text)
+
+
+
