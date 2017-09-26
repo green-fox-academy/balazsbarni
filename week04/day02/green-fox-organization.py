7850982baff0a747d300780ea989585f92b37bf3
BalazsBarna <balazsbarni@gmail.com>
2017-09-26 16:13:39 +0200

    feat(GreenFoxOrg):ExerciseCompleted

--- week04/day02/green-fox-organization.py
+++ week04/day02/green-fox-organization.py
@@ -28,7 +28,7 @@ class Student(Person):
         
 
 class Mentor(Person):
-    def __init__(self, name, age, gender, level="Intermediate"):
+    def __init__(self, name="Jane Doe", age=30, gender="female", level="Intermediate"):
         super().__init__(name, age, gender)
         self.level = level
     
@@ -38,8 +38,9 @@ class Mentor(Person):
     def introduce(self):
         print("Hi, I\'m {}, a {} year old {} {} mentor.".format(self.name, self.age, self.gender, self.level))
 
+
 class Sponsor(Person):
-    def __init__(self, name, age, gender, company="Google", hired_students=0):
+    def __init__(self, name="Jane Doe", age=30, gender="female", company="Google", hired_students=0):
         super().__init__(name, age, gender)
         self.company = company
         self.hired_students = hired_students
@@ -50,24 +51,59 @@ class Sponsor(Person):
     def hire(self):
         self.hired_students += 1
         
-    
     def get_goal(self):
         print("Hire brilliant junior software developers.")
 
-class Pallida():
-    def __init__(self, class_name, students, mentors):
-        pass
-    
-    def add_student():
-        pass
-    
-    def add_mentor():
-        pass
-    
-    def info():
-        pass
-    
 
-csabi = Student("bela")
-csabi.introduce()
-csabi.get_goal()
+class PallidaClass(object):
+    def __init__(self, class_name, students=0, mentors=0):
+        self.class_name = class_name
+        self.students = []
+        self.mentors = []
+    
+    def add_student(self, student):
+        self.students.append(student)
+        
+    def add_mentor(self, mentor):
+        self.mentors.append(mentor)
+        
+    def info(self):
+        print("Pallida {} class has {} and {} mentors.".format(self.class_name, len(self.students), len(self.mentors)))
+
+
+# Test input
+people = []
+
+mark = Person('Mark', 46, 'male')
+people.append(mark)
+jane = Person()
+people.append(jane)
+john = Student('John Doe', 20, 'male', 'BME')
+people.append(john)
+student = Student()
+people.append(student)
+gandhi = Mentor('Gandhi', 148, 'male', 'senior')
+people.append(gandhi)
+mentor = Mentor()
+people.append(mentor)
+sponsor = Sponsor()
+elon = Sponsor('Elon Musk', 46, 'male', 'SpaceX')
+people.append(elon)
+student.skip_days(3)
+
+for i in range(5):
+    elon.hire()
+
+for i in range(3):
+    sponsor.hire()
+
+for member in people:
+    member.introduce()
+    member.get_goal()
+
+badass = PallidaClass('BADA55')
+badass.add_student(student)
+badass.add_student(john)
+badass.add_mentor(mentor)
+badass.add_mentor(gandhi)
+badass.info()
