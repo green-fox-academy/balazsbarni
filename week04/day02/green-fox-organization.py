d1727469808e11c358a51788468397b79b708d30
BalazsBarna <balazsbarni@gmail.com>
2017-09-26 11:30:38 +0200

    feat(GreenFoxOrg):ProjectSkeletonCreated

---
+++ week04/day02/green-fox-organization.py
@@ -0,0 +1,59 @@
+class Person():
+    def __init__(self, name, age, gender):
+        pass
+    
+    def introduce(self):
+        pass
+    
+    def get_goal():
+        pass
+
+class Student(Person):
+    def __init__(self, previous_organization, skipped_days):
+        pass
+    
+    def get_goal():
+        pass
+    
+    def introduce():
+        pass
+    
+    def skip_days():
+        pass
+
+class Mentor(Person):
+    def __init__(self, level):
+        pass
+    
+    def get_goal():
+        pass
+    
+    def introduce():
+        pass
+
+class Sponsor(Person):
+    def __init__(self, company, hired_students):
+        pass
+    
+    def introduce():
+        pass
+    
+    def hire():
+        pass
+    
+    def get_goal():
+        pass
+
+class Pallida():
+    def __init__(self, class_name, students, mentors):
+        pass
+    
+    def add_student():
+        pass
+    
+    def add_mentor():
+        pass
+    
+    def info():
+        pass
+    
