4690e4d6cf048fbfe0da21fcacb764b3702d27f6
BalazsBarna <balazsbarni@gmail.com>
2017-09-27 12:26:49 +0200

    feat(Sum): Example Ended

--- week04/day03/barna_balazs_test.py
+++ week04/day03/barna_balazs_test.py
@@ -1,11 +1,32 @@
 import unittest
-from barna_balazs_work import Apples
+from barna_balazs_work import My_Class
+test_sum = My_Class()
 
-class Apples_test(unittest.TestCase):
+
+class My_Class_Test(unittest.TestCase):
     def test_get_apple(self):
-        test_apple = Apples()
+        test_apple = My_Class()
         self.assertEquals(test_apple.get_apple(), "apple")
 
+    def test_get_sum_list_with_items(self):
+        my_num = [2,3]
+        self.assertEquals(test_sum.get_sum(my_num), 5)
+
+    def test_get_sum_empty_list(self):
+        empty_list = []
+        self.assertEquals(test_sum.get_sum(empty_list), 0)
+
+    def test_my_sum_one_element(self):
+        one_item_list = [2]
+        self.assertEquals(test_sum.get_sum(one_item_list), 2)
+
+    def test_my_sum_multiple_element(self):
+        multiple_item_list = [1, 2, 3, 4]
+        self.assertEquals(test_sum.get_sum(multiple_item_list), 10)
+
+    def test_my_sum_null(self):
+        null_list = [0]
+        self.assertEquals(test_sum.get_sum(null_list), 0)
 
 if __name__ == '__main__':
     unittest.main()
--- week04/day03/barna_balazs_work.py
+++ week04/day03/barna_balazs_work.py
@@ -1,3 +1,14 @@
-class Apples(object):
+class My_Class(object):
+    
     def get_apple(self):
         return "apple"
+
+
+    def get_sum(self, list_to_sum):
+        sum_of_list = 0
+        for items in list_to_sum:
+            sum_of_list += items
+        return sum_of_list
+
+
+
