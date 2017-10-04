7fcf60588ea1c3feba4a74957bc20894978192f5
BalazsBarna <balazsbarni@gmail.com>
2017-10-04 15:39:22 +0200

    feat(Rpg): Skeleton and Boss added

--- week05/week-project-rpg/main-rpg.py
+++ week05/week-project-rpg/main-rpg.py
@@ -39,8 +39,29 @@ class Map(object):
         if 0 <= x <= 9 and 0 <= y <= 9:
             if self.map_plan[y][x] == 0:
                 return True
-        
-class Hero:
+
+class Entity(object):
+    size = 72
+    def __init__(self, coord_x, coord_y, entity_image):
+        self.entity = None
+        self.coord_x = coord_x
+        self.coord_y = coord_y
+        self.entity_image = entity_image
+
+
+    def create_entity(self):
+        x = self.coord_x * self.size + self.size/2
+        y = self.coord_y * self.size + self.size/2
+        self.entity = Map.canvas.create_image(x, y, image = self.entity_image)
+    
+    def move(self, x, y):
+        self.coord_x += x 
+        self.coord_y += y
+        Map.canvas.move(self.entity, self.size * x , self.size * y)
+
+    
+
+class Hero(Entity):
     size = 72
     hero_down = PhotoImage(file = "hero-down.gif")
     hero_up = PhotoImage(file = "hero-up.gif")
@@ -49,27 +70,42 @@ class Hero:
     
     def __init__(self):
         self.hero = None
-        self.coord_x = 0
-        self.coord_y = 0
-
-    def create_hero(self):
-        x = self.size/2
-        y = self.size/2
-        self.hero = Map.canvas.create_image(x, y, image = self.hero_down)
+        super().__init__(0, 0, self.hero_down)
 
     def update_image(self, new_image):
         Map.canvas.itemconfig(self.hero, image = new_image)
-    
-    def move(self, x, y):
-        self.coord_x += x 
-        self.coord_y += y
-        Map.canvas.move(self.hero, self.size * x , self.size * y)
+
+class Skeleton(Entity):
+    size = 72
+    skeleton_image = PhotoImage(file = "skeleton.gif")
+
+    def __init__(self):
+        self.skeleton = None
+        self.skeleton_x = 1
+        self.skeleton_y = 1
+        super().__init__(self.skeleton_x, self.skeleton_y, self.skeleton_image)
+
+class Boss(Entity):
+    size = 72
+    boss_image = PhotoImage(file = "boss.gif")
+
+    def __init__(self):
+        self.boss = None
+        self.boss_x = 1
+        self.boss_y = 0
+        super().__init__(self.boss_x, self.boss_y, self.boss_image)
+
+
 
 
 hero = Hero()
+skeli = Skeleton()
+boss = Boss()
 map = Map()
 map.draw_full_map()
-hero.create_hero()
+hero.create_entity()
+skeli.create_entity()
+boss.create_entity()
 
 
 def on_key_press(e):
