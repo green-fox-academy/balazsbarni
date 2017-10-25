acadfd8e80e7a1880e07af3e16344b76e8693fa9
BalazsBarna <balazsbarni@gmail.com>
2017-10-25 16:39:36 +0200

    feat(Strings02):Complete

---
+++ week07/day03/strings02.js
@@ -0,0 +1,16 @@
+'use strict';
+// Reverse the string!
+
+var reversed = ".eslaf eb t'ndluow ecnetnes siht ,dehctiws erew eslaf dna eurt fo sgninaem eht fI"
+
+//let revString = reversed.split('');
+//let revArray = revString.reverse();
+//reversed = revArray.join('');
+
+let rev =  reversed.split('').reverse().join('');
+
+
+
+//console.log(reversed);
+
+console.log(rev);
