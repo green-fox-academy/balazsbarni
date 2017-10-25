7a57a4c0a0dd2e51c7af27bccb21c558f2a17c49
BalazsBarna <balazsbarni@gmail.com>
2017-10-25 14:28:43 +0200

    feat(ArrayAppendA):Complete

---
+++ week07/day03/array_append_a.js
@@ -0,0 +1,13 @@
+'use strict';
+// - Create a variable named `nimals`
+//   with the following content: `["kuty", "macsk", "cic"]`
+// - Add all elements an `"a"` at the end
+// - try to use built in functions instead of loops
+
+let nimals = ["kuty", "macsk", "cic"];
+
+let newArr = nimals.map(function(element){
+    return element += 'a';
+})
+
+console.log(newArr);
