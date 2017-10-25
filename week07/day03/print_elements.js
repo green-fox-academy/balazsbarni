ef9d8009e9a929f3fe30b5d8843296e0b40c2ca8
BalazsBarna <balazsbarni@gmail.com>
2017-10-25 13:49:48 +0200

    feat(PrintElements):Complete

---
+++ week07/day03/print_elements.js
@@ -0,0 +1,14 @@
+'use strict';
+// - Create a variable named `af` with the following content: `[4, 5, 6, 7]`
+// - Log each the element of `af` to the console*
+// *hint: use a loop, console.log(af) won't cut it
+// - bonus for using the correct built in array method
+
+let af = [4, 5, 6, 7];
+let toLog = '';
+
+af.forEach(function(values) {
+    toLog += values + ' ';
+})
+
+console.log(toLog);
