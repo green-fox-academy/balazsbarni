27fb73b2baa36ab46dc5f8aacc3d42330ecc9891
BalazsBarna <balazsbarni@gmail.com>
2017-10-30 11:39:31 +0100

    feat(MoneyShuffle2): Another solution

---
+++ week08/day01/money_shuffle_2.js
@@ -0,0 +1,37 @@
+let Shuffler = {
+    cash: 10000,
+    transactionCount: 1,
+    pick: function() {
+        this.cash -= 1000;
+        this.target = this.transactionCount % 2 === 1 ? Panama : Cyprus;
+        this.target.deposit(1000);
+        this.transactionCount += 1;
+        console.log(this.target.name  + ' got 1000!');
+    }
+}
+
+let Panama = {
+    cash: 0,
+    name: 'Panama',
+    tax: '1%',
+    deposit: function() {
+        this.cash += 1000;
+    }
+}
+
+let Cyprus = {
+    cash: 0,
+    name: 'Cyprus',
+    tax: '5%',
+    deposit: function() {
+        this.cash += 1000;
+    }
+}
+
+Shuffler.pick();
+Shuffler.pick();
+Shuffler.pick();
+Shuffler.pick();
+
+console.log(Panama.cash);
+console.log(Cyprus.cash);
