85ad0e516c3ec4a9dcecaedc82dc67b88c887556
BalazsBarna <balazsbarni@gmail.com>
2017-10-30 13:26:33 +0100

    feat(Sharpie):Example complete

---
+++ week08/day01/sharpie.js
@@ -0,0 +1,24 @@
+'use strict';
+
+class Sharpie {
+    constructor(color, width) {
+        this.color = color;
+        this.width = width;
+        this.inkAmount = 100;
+    }
+
+    use() {
+        this.inkAmount -= this.width;
+    }
+};
+
+let blueSharp = new Sharpie('blue', 10);
+
+let useSharpie = function(sharpie) {
+    while (sharpie.inkAmount > 0) {
+        sharpie.inkAmount -= sharpie.width;
+    };
+};
+
+useSharpie(blueSharp);
+console.log(blueSharp.inkAmount);
