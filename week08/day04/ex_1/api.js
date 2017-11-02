f28c9d58160be76de7ce92b43cee01b08060fb7c
BalazsBarna <balazsbarni@gmail.com>
2017-11-02 15:17:49 +0100

    feat(Gifs)Ended

--- week08/day04/ex_1/api.js
+++ week08/day04/ex_1/api.js
@@ -1,43 +1,23 @@
 'use strict';
 
-function getGifs () {
+function getGifs (readyFunc) {
     let xhr = new XMLHttpRequest();
-    xhr.open('GET', 'https://api.giphy.com/v1/gifs/search?api_key=BpCwzxMUOebR3q1jR6RcHS5WtMAg3gMG&q=star wars&limit=16&offset=0&rating=G&lang=en');
+    xhr.open('GET', 'https://api.giphy.com/v1/gifs/search?api_key=BpCwzxMUOebR3q1jR6RcHS5WtMAg3gMG&q=dogs&limit=16&offset=0&rating=G&lang=en');
     xhr.onreadystatechange = function() {
         if(xhr.readyState == 4) {
-            let id = 0;
             let gif = JSON.parse(xhr.responseText);
-            for (let i = 0; i < gif.data.length; i ++) {
-                document.getElementById(i).setAttribute('src', gif.data[i].images.fixed_width_still.url);
-                id ++;
-            };
+            readyFunc(gif)
         };
     };
     xhr.send();
 };
 
-function getGifsMove(id) {
-    let xhr = new XMLHttpRequest();
-    xhr.open('GET', 'https://api.giphy.com/v1/gifs/search?api_key=BpCwzxMUOebR3q1jR6RcHS5WtMAg3gMG&q=star wars&limit=16&offset=0&rating=G&lang=en');
-    xhr.onreadystatechange = function() {
-        if(xhr.readyState == 4) {
-            let gif = JSON.parse(xhr.responseText);
-            document.getElementById(id).setAttribute('src', gif.data[id].images.fixed_width.url);
-        };
-    };
-    xhr.send();
-};
-
-let imgArr = document.querySelectorAll('img')
-for (let i = 0; i < imgArr.length; i ++) {
-    document.getElementById(i).addEventListener('click', function(){
-        getGifsMove(i);
-    });
-};
-
-
-getGifs();
-
-
-
-
+getGifs(function(items){
+    let imgArr = document.querySelectorAll('img')
+    for (let i = 0; i < items.data.length; i++) { 
+        imgArr[i].setAttribute('src', items.data[i].images.fixed_width_still.url);
+        imgArr[i].addEventListener('click', function(){
+            imgArr[i].setAttribute('src', items.data[i].images.fixed_width.url);
+        });   
+    } 
+});
