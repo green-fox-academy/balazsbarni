8663895c110da722b0e5a1173717c1916dd98aef
BalazsBarna <balazsbarni@gmail.com>
2017-11-02 16:06:33 +0100

    feat(Gifs)Ended

--- week08/day04/ex_1/api.js
+++ week08/day04/ex_1/api.js
@@ -5,19 +5,24 @@ function getGifs (readyFunc) {
     xhr.open('GET', 'https://api.giphy.com/v1/gifs/search?api_key=BpCwzxMUOebR3q1jR6RcHS5WtMAg3gMG&q=dogs&limit=16&offset=0&rating=G&lang=en');
     xhr.onreadystatechange = function() {
         if(xhr.readyState == 4) {
-            let gif = JSON.parse(xhr.responseText);
-            readyFunc(gif)
+            let gif = JSON.parse(xhr.responseText).data;
+            gif.forEach(readyFunc)
         };
     };
     xhr.send();
 };
 
-getGifs(function(items){
-    let imgArr = document.querySelectorAll('img')
-    for (let i = 0; i < items.data.length; i++) { 
-        imgArr[i].setAttribute('src', items.data[i].images.fixed_width_still.url);
-        imgArr[i].addEventListener('click', function(){
-            imgArr[i].setAttribute('src', items.data[i].images.fixed_width.url);
-        });   
-    } 
+let renderGif = (function(item){
+       let tempImg = document.createElement('img');
+       tempImg.dataset.pista = item.images.fixed_width.url
+       tempImg.src = item.images.fixed_width_still.url;
+       tempImg.addEventListener('click', function(){
+           [tempImg.src, tempImg.dataset.pista] = [tempImg.dataset.pista, tempImg.src]
+        //    let tempData = tempImg.src
+        //    tempImg.src = tempImg.dataset.pista;
+        //    tempImg.dataset.pista = tempData;
+       });
+       document.body.appendChild(tempImg); 
 });
+
+getGifs(renderGif);
