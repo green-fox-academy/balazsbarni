9fd5e2c27799c4996582385e77a39cdeded7fa6c
BalazsBarna <balazsbarni@gmail.com>
2017-11-08 11:54:44 +0100

    feat(Backend):server create

---
+++ week09/day03/node_project/backend.js
@@ -0,0 +1,11 @@
+'use strict';
+
+var express = require('express');
+var app = express();
+app.use(express.static('assets'))
+
+app.get('/', function(req, res) {
+    res.sendFile(__dirname + '/index.html');    
+});
+
+app.listen(8080);
