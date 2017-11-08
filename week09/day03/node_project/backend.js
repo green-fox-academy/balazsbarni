e20e9450aec3c71057fe55faaac9279c3902c652
BalazsBarna <balazsbarni@gmail.com>
2017-11-08 15:19:19 +0100

    feat(Backend): Doubling resolved

--- week09/day03/node_project/backend.js
+++ week09/day03/node_project/backend.js
@@ -2,10 +2,25 @@
 
 var express = require('express');
 var app = express();
-app.use(express.static('assets'))
+app.use('/assets', express.static('assets'));
 
 app.get('/', function(req, res) {
     res.sendFile(__dirname + '/index.html');    
+});
+
+app.get('/doubling', function(req, res) {
+    var toDoubleAnything = req.query.input;
+    let toDouble = parseInt(toDoubleAnything);
+    if (!toDouble){
+        res.json({
+            "error": "Please provide an input!"
+        });
+    } else {
+        res.json({
+            'received' : toDouble,
+            'result' : toDouble * 2
+        });
+    };
 });
 
 app.listen(8080);
