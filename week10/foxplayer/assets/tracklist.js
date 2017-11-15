52e82b9e9e0249bed6d1b90d77e15d67c3175b81
BalazsBarna <balazsbarni@gmail.com>
2017-11-15 17:30:20 +0100

    feat(FoxPlayer):tracklist add

---
+++ week10/foxplayer/assets/tracklist.js
@@ -0,0 +1,30 @@
+'use strict';
+
+const trackList = (function() {
+    let render = function(item) {
+        let counter = 1;
+        console.log(item)
+        item.forEach(function(item){
+            let temp = document.querySelector('.tracks');
+            let rendered = document.createElement('div');
+            rendered.innerText = counter + '  ' + item.title + ' (' + item.artist + ') ' + convertSecs(item.duration);
+            rendered.className = 'track';
+            temp.appendChild(rendered);
+            counter ++;
+        });
+    };
+
+    let convertSecs = function(s){
+        return(s-(s%=60))/60+(9<s?':':':0')+s
+    }
+
+    let load = function() {
+        ajax('GET', 'tracks', render);
+    };
+
+    return {
+        load: load
+    };
+})();
+
+trackList.load();
